f82eb639b0f3b97490edfb07d38f41be6db3ccfa
WesleyCurtin <curtinwl@mail.uc.edu>
2022-09-22 13:29:38 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int month = 3;
+
+        if (month >= 1, month <= 12) {
+        System.out.println("Your birth month is: " + month);
+    }   else {
+        System.out.println("You entered an incorrect month value: " + month);
+    }
+
+
+
+
+        
+    }
+}
